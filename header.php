fff6cc4234ef79b275646f7150d0aa2b307dcac8
Frank Barrera <frankbarreraag@gmail.com>
2023-11-16 11:18:19 -0500

    agregando archivos php

---
+++ header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The header for the OnePress theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package OnePress
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-100932625-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<?php do_action( 'onepress_before_site_start' ); ?>
+<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'onepress' ); ?></a>
+    <?php
+    /**
+     * Hooked: onepress_site_header
+     *
+     * @see onepress_site_header
+     */
+    do_action( 'onepress_site_start' );
+    ?>
